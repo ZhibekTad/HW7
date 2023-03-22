3a314f2e3c83c621898cd532a9dfffae34d719cf
Zhibekt Tadzhibayeva <jibektad@mail.ru>
2023-03-22 20:34:00 +0600

    Initial Commit

---
+++ Task2/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента 
+// в двумерном массиве, и возвращает значение этого элемента или же указание, 
+// что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+int Prompt(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] CreateArray(int m, int n)
+{
+    int[,] array = new int [m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = i + j;
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    return array;
+}
+int m = Prompt("Введите количество строк: ");
+int n = Prompt("Введите количество столбцов: ");
+Console.WriteLine();
+
+int[,] numbers = CreateArray(m, n);
+int z = Prompt("Введите позицию элемента в строке: ");
+int y = Prompt("Введите позицию элемента в столбце: ");
+if (z > numbers.GetLength(0) || y > numbers.GetLength(1))
+{
+    Console.WriteLine("такого элемента нет");
+}
+else
+{
+    Console.WriteLine($"значение элемента {z} строки и {y} столбца равно {numbers[z-1,y-1]}");
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int Prompt(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] PrintArray(int m, int n)
+{
+    int[,] array = new int [m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = i + j;
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    return array;
+}
+
+int m = Prompt("Введите количество строк: ");
+int n = Prompt("Введите количество столбцов: ");
+Console.WriteLine();
+
+PrintArray(m, n);
+
+int[,] aveNum = new int[m, n];;
+for (int j = 0; j < aveNum.GetLength(1); j++)
+{
+   double average = 0;
+   for (int i = 0; i < aveNum.GetLength(0); i++)
+    {
+        average = (average + aveNum[j, i]); // не пойму отчего не считает среднее
+    }
+   average = average / n;
+   Console.Write(average + "; ");
+}
+Console.WriteLine();
+
